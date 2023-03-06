26f1c1a224caa6efac62281823b3831fccea293d
janakraj7447 <janak.raj@netsolutions.com>
2023-03-06 13:55:53 +0530

    validation

--- Models/EmployeeDetailsModel.cs
+++ Models/EmployeeDetailsModel.cs
@@ -2,16 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Assignment.Models
 {
     public class EmployeeDetailsModel
     {
-        public int EmpId {get;set;}
-        public string ?Name {get; set;}
-        public string ?Age {get;set;}
-        public string ?salary{get;set;}
-        public string ?Designation{get;set;}
+        public int EmpId { get; set; }
+        [Required]
+         [MaxLength(10)]
+        public string? Name { get; set; }
+        [Required]
+        [Range(1,100, ErrorMessage ="Age must be between 1-100 in years.")]
+        public string? Age { get; set; }
+        [Required]
+         [Range(1,100000, ErrorMessage ="Salary must be between 1-100000.")]
+        public string? salary { get; set; }
+        [Required]
+          [MaxLength(10)]
+        public string? Designation { get; set; }
 
     }
 }
